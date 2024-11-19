89a63e6e0331f77b12d8ff8556409a2eb0fb2d4d
Alfredo hernandez <arlequine666.ah@gmail.com>
2024-11-19 17:20:30 -0600

    fix(*):fix styles outline

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -4,93 +4,37 @@ import { useTaskContext } from '../context/TaskContext';
 
 const TaskList: React.FC = () => {
   const { tasks, editTask, deleteTask, toggleTaskCompletion } = useTaskContext();
-  const [editingTaskId, setEditingTaskId] = useState<string | null>(null);
-  const [newTitle, setNewTitle] = useState('');
+  const [newTitle, setNewTitle] = useState<{ [key: string]: string }>({});
 
-  const handleEdit = (taskId: string, currentTitle: string) => {
-    if (editingTaskId === taskId) {
-      editTask(taskId, newTitle || currentTitle);
-      setEditingTaskId(null);
-      setNewTitle('');
-    } else {
-      setEditingTaskId(taskId);
-      setNewTitle(currentTitle);
-    }
+  const handleChange = (taskId: string, value: string) => {
+    setNewTitle((prev) => ({ ...prev, [taskId]: value }));
   };
 
   return (
     <div className="mt-4">
-      <h2 className="text-xl font-bold mb-2">Lista de Tareas</h2>
-      <table className="min-w-full border-collapse border border-gray-200">
-        <thead>
-          <tr>
-            <th className="border border-gray-300 p-2">#</th>
-            <th className="border border-gray-300 p-2">Tarea</th>
-            <th className="border border-gray-300 p-2">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task, index) => (
-            <tr key={task.id} className="border border-gray-300">
-              <td className="border border-gray-300 p-2">{index + 1}</td>
-              <td className="border border-gray-300 p-2">
-                {editingTaskId === task.id ? (
-                  <>
-                    <div className="flex flex-col md:flex-row md:items-center">
-                      <input 
-                        type="text" 
-                        value={newTitle} 
-                        onChange={(e) => setNewTitle(e.target.value)} 
-                        className="border p-1 w-full md:w-1/2 ml-2"
-                      />
-                      <div className="flex items-center">
-                        <label className="relative inline-flex items-center cursor-pointer">
-                          <input 
-                            type="checkbox" 
-                            checked={task.completed} 
-                            onChange={() => toggleTaskCompletion(task.id)} 
-                            className="sr-only peer"
-                          />
-                          <div className={`w-11 h-6 rounded-full transition-colors ${task.completed ? 'bg-green-500' : 'bg-red-500'}`}></div>
-                          <div className="absolute w-5 h-5 bg-white rounded-full shadow-md left-1 top-0.5 transition-transform peer-checked:translate-x-full"></div>
-                        </label>
-                        <label className="text-sm md:w-1/2">Completar</label>
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <span className={task.completed ? 'line-through' : ''} title={task.completed ? 'Completada' : 'No completada'}>
-                    {task.title}
-                  </span>
-                )}
-              </td>
-              <td className="border border-gray-300 p-2">
-                {editingTaskId === task.id ? (
-                  <button 
-                    onClick={() => handleEdit(task.id, task.title)} 
-                    className="bg-blue-500 text-white p-1 mx-1"
-                  >
-                    Guardar
-                  </button>
-                ) : (
-                  <button 
-                    onClick={() => handleEdit(task.id, task.title)} 
-                    className="bg-yellow-500 text-white p-1 mx-1"
-                  >
-                    Editar
-                  </button>
-                )}
-                <button 
-                  onClick={() => deleteTask(task.id)} 
-                  className="bg-red-500 text-white p-1 mx-1"
-                >
-                  Eliminar
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <ul className="list-none">
+        {tasks.map((task) => (
+          <li key={task.id} className="flex bg-transparent items-center justify-start border-b-2 border-b-gray-400  py-2 w-full">
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTaskCompletion(task.id)}
+              className="mr-2 w-auto"
+            />
+            <input
+              type="text"
+              value={newTitle[task.id] || task.title}
+              onChange={(e) => handleChange(task.id, e.target.value)}
+              onBlur={() => editTask(task.id, newTitle[task.id] || task.title)}
+              className="bg-transparent border-none text-white border-none bg-transparent focus:outline-none focus:border-b-2 w-auto"
+              placeholder="Agregar nueva tarea"
+            />
+            <button onClick={() => deleteTask(task.id)} className="bg-red-500 text-white p-1 mx-1">
+              Eliminar
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
